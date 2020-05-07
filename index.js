3de9932ea884f1eed235b70b3f22be339bae8fc2
Hong Yue Wang <hongyuew26@gmail.com>
2020-05-07 18:33:53 -0400

    Made ping command case-insensitive and now uses keyword research

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const config = require('./config.json');
 bot.login(config.token);
 
 bot.on('message', (message) => {
-    if(message.content == 'ping') {
+    if(message.content.toLowerCase().includes('ping')
+    && message.content.includes("Tu veux savoir ton ping? " +
+    "Un peu comme le ping pong? HAHAHA PING PONG COMME LE SPORT.") == false) {
         message.reply('Tu veux savoir ton ping? Un peu comme le ping pong? HAHAHA PING PONG COMME LE SPORT.');
     }
 });
-
-//
