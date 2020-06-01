28197dea65adc51c8cfaa92fd41a41dfcb067b31
Hong Yue Wang <hongyuew26@gmail.com>
2020-05-31 23:13:34 -0400

    Implemented the quiz command

--- commands/games/ball.js
+++ commands/games/ball.js
@@ -18,63 +18,63 @@ module.exports = class Ball extends Command {
         .setColor('#0099ff')
         .setTitle('Cédric est en possession du ballon.')
         .setDescription('Va-t-il faire une passe ou tirer?')
-        .attachFiles(['./commands/games/basketball.jpg'])
+        .attachFiles(['./commands/games/ball-img/basketball.jpg'])
         .setImage('attachment://basketball.jpg');
 
         const embedPass = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Cédric vous passe la balle.')
         .setDescription('"Vas-y, fais un panier! Comme à la NBA!"')
-        .attachFiles(['./commands/games/cedric_pass.jpg'])
+        .attachFiles(['./commands/games/ball-img/cedric_pass.jpg'])
         .setImage('attachment://cedric_pass.jpg');
 
         const embedNoStimuli = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Cédric ne répond pas aux stimuli.')
         .setDescription('... La game est finie.')
-        .attachFiles(['./commands/games/basketball.jpg'])
+        .attachFiles(['./commands/games/ball-img/basketball.jpg'])
         .setImage('attachment://basketball.jpg');
 
         const embedCedricShot = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Cédric lance le ballon vers le panier, espérant faire un point.')
         .setDescription('"Yooooo, check la balle aller!"')
-        .attachFiles(['./commands/games/shoot.gif'])
+        .attachFiles(['./commands/games/ball-img/shoot.gif'])
         .setImage('attachment://shoot.gif');
 
         const embedUserShot = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Vous faites un lancer. Cédric encourage le ballon.')
         .setDescription(`"Aweille let's go le ballon!"`)
-        .attachFiles(['./commands/games/user_shoot.jpg'])
+        .attachFiles(['./commands/games/ball-img/user_shoot.jpg'])
         .setImage('attachment://user_shoot.jpg');
 
         const embedCedricPoint = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Cédric marque un point.')
         .setDescription(`"Oh ça c'était un bon lancer! Je suis fier de mon coup!"`)
-        .attachFiles(['./commands/games/point.jpg'])
+        .attachFiles(['./commands/games/ball-img/point.jpg'])
         .setImage('attachment://point.jpg');
 
         const embedCedricMiss = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Cédric manque le panier.')
         .setDescription(`"It's hokay, je l'aurai la prochaine fois."`)
-        .attachFiles(['./commands/games/miss.jpg'])
+        .attachFiles(['./commands/games/ball-img/miss.jpg'])
         .setImage('attachment://miss.jpg');
 
         const embedUserPoint = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Vous marquez un point.')
         .setDescription(`"Heille bravo ${message.author.username}. C'était vraiment beau ça."`)
-        .attachFiles(['./commands/games/point.jpg'])
+        .attachFiles(['./commands/games/ball-img/point.jpg'])
         .setImage('attachment://point.jpg');
 
         const embedUserMiss = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Vous manquez le panier.')
         .setDescription(`"Come on ${message.author.username}, t'es poche!"`)
-        .attachFiles(['./commands/games/miss.jpg'])
+        .attachFiles(['./commands/games/ball-img/miss.jpg'])
         .setImage('attachment://miss.jpg');
 
         message.say("Le basket? Ye je suis down de jouer.");
---
+++ commands/games/quiz.js
@@ -0,0 +1,32 @@
+const { Command } = require('discord.js-commando');
+const Discord = require('discord.js');
+const quiz = require('./quiz-questions.json');
+
+module.exports = class Quiz extends Command {
+    constructor(bot) {
+        super(bot, {
+            name: 'quiz',
+            group: 'games',
+            memberName: 'quiz',
+            description: 'A quiz about various Quebecer things.'
+        });
+    }
+
+    async run(message) {
+        const item = quiz[Math.floor(Math.random() * quiz.length)];
+        const filter = response => {
+            return item.answers.some(answer => answer.toLowerCase() ===
+            response.content.toLowerCase());
+        };
+
+        message.channel.send(item.question).then(() => {
+            message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time']})
+            .then(collected => {
+                message.channel.send(`Heille, ${collected.first().author} a eu la bonne réponse! Wow t'es smart!`);
+            })
+            .catch(collected => {
+                message.channel.send(`Ça a l'air que personne a eu la bonne réponse... Come on, vous aimez pas le Québec...`);
+            });
+        });
+    }
+}
