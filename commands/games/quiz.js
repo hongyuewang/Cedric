917ec48d9eaf3da5e87e93a75a98f9e25a320f81
Hong Yue Wang <hongyuew26@gmail.com>
2020-06-01 11:49:24 -0400

    Added rounds to quiz

--- commands/games/quiz.js
+++ commands/games/quiz.js
@@ -13,24 +13,42 @@ module.exports = class Quiz extends Command {
     }
 
     async run(message) {
-        const item = quiz[Math.floor(Math.random() * quiz.length)];
-        const filter = response => {
-            return item.answers.some(answer => answer.toLowerCase() ===
-            response.content.toLowerCase());
-        };
+        var questionsAsked = 0;
 
-        message.channel.send(item.question).then(() => {
-            if (item.image != undefined) {
-                const embed = new Discord.MessageEmbed().setImage(item.image);
-                message.channel.send(embed);
-            }
-            message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time']})
-            .then(collected => {
-                message.channel.send(`Heille, ${collected.first().author} a eu la bonne réponse! Wow t'es smart!`);
-            })
-            .catch(collected => {
-                message.channel.send(`Ça a l'air que personne a eu la bonne réponse... Come on, vous aimez pas le Québec...`);
+        play();
+
+        function play() {
+            const item = quiz[Math.floor(Math.random() * quiz.length)];
+            const filter = response => {
+                return item.answers.some(answer => answer.toLowerCase() ===
+                response.content.toLowerCase());
+            };
+
+            message.channel.send(item.question).then(() => {
+                if (item.image != undefined) {
+                    const embed = new Discord.MessageEmbed().setImage(item.image);
+                    message.channel.send(embed);
+                }
+                message.channel.awaitMessages(filter, {max: 1, time: 30000, errors: ['time']})
+                .then(collected => {
+                    message.channel.send(`Heille, ${collected.first().author}, t'as eu la bonne réponse! T'es smart!`);
+                    questionsAsked += 1;
+                    if (questionsAsked < quiz.length) {
+                        play();
+                    } else {
+                        message.channel.send("Bin kin, c'était une bonne game les gars! Je vous félicite pour vos connaissances sur le Québec!");
+                    }
+                })
+                .catch(collected => {
+                    message.channel.send(`Ça a l'air que personne a eu la bonne réponse... Come on, vous aimez pas le Québec...`);
+                    questionsAsked += 1;
+                    if (questionsAsked < quiz.length) {
+                        play();
+                    } else {
+                        message.channel.send("Bin kin, c'était une bonne game les gars! Je vous félicite pour vos connaissances sur le Québec!");
+                    }
+                });
             });
-        });
+        }
     }
 }
