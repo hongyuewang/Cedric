5cddeb3978162e81ef25ee9f9c8142cea5f43fb6
Hong Yue Wang <hongyuew26@gmail.com>
2020-06-05 18:34:52 -0400

    Add point system to quiz command, disable question-repeating and add new trivia questions

--- commands/games/quiz.js
+++ commands/games/quiz.js
@@ -2,6 +2,33 @@ const { Command } = require('discord.js-commando');
 const Discord = require('discord.js');
 const quiz = require('./quiz-questions.json');
 
+class Player {
+    constructor(username) {
+        this._username = username;
+        this._score = 0;
+    }
+
+    get username() {
+        return this._username;
+    }
+
+    set username(username) {
+        this._username = username;
+    }
+
+    get score() {
+        return this._score;
+    }
+
+    set score(newScore) {
+        this._score = newScore;
+    }
+
+    addPoint() {
+        this._score += 1;
+    }
+}
+
 module.exports = class Quiz extends Command {
     constructor(bot) {
         super(bot, {
@@ -14,16 +41,37 @@ module.exports = class Quiz extends Command {
 
     async run(message) {
         var questionsAsked = 0;
+        let playerArray = [];
 
         // Starts the quiz
+        message.channel.send(`**Bienvenue à mon quiz!!!**`);
+        message.channel.send("**Vous avez 30 secondes pour répondre à chaque question!**");
+        await new Promise(resolve => setTimeout(resolve, 2000));
         play();
 
         function play() {
-            const item = quiz[Math.floor(Math.random() * quiz.length)];
+            var randomIndex = Math.floor(Math.random() * quiz.length);
+            var item = quiz[randomIndex];
             const filter = response => {
                 return item.answers.some(answer => answer.toLowerCase() ===
                 response.content.toLowerCase());
             };
+
+            const allMessagesFilter = m => m.author.id != "708042232292180011";
+
+            const allCollector = message.channel.createMessageCollector(allMessagesFilter, { time: 3015000 });
+
+            allCollector.on('collect', m => {
+	               console.log(`Collected ${m.content}`);
+                   if (playerArray.find(obj => obj.username == m.author.username) == undefined) {
+                       playerArray.push(new Player(m.author.username));
+                       console.log('New Player!');
+                       console.log(playerArray);
+                   } else {
+                       console.log('Current Players:');
+                       console.log(playerArray);
+                   }
+            });
 
             message.channel.send(item.question).then(() => {
                 if (item.image != undefined) {
@@ -37,13 +85,20 @@ module.exports = class Quiz extends Command {
                     if (item.alternativeAnswer != undefined) {
                         message.channel.send(`**${item.alternativeAnswer}** était aussi acceptable.`);
                     }
-
+                    let roundWinner = playerArray.find(obj => obj.username == collected.first().author.username);
+                    roundWinner.addPoint();
                     questionsAsked++;
+                    console.log(questionsAsked);
 
-                    if ( questionsAsked < quiz.length) {
+                    if ( questionsAsked < 5) {
                         play();
                     } else {
                         message.channel.send("Bin kin, c'était une bonne game les gars! Je vous félicite pour vos connaissances sur le Québec!");
+                        message.channel.send("Voici le leaderboard de cette partie. Ou plutôt le tableau de classement parce que je suis québécois.");
+                        message.channel.send("**TABLEAU DE CLASSEMENT**");
+                        for (var player of playerArray) {
+                            message.channel.send(`**${player.username}**: ${player.score} points`);
+                        }
                     }
                 })
                 .catch(collected => {
@@ -55,14 +110,19 @@ module.exports = class Quiz extends Command {
 
                     questionsAsked++;
 
-                    if (questionsAsked < quiz.length) {
+                    if (questionsAsked < 5) {
                         play();
                     } else {
                         message.channel.send("Bin kin, c'était une bonne game les gars! Je vous félicite pour vos connaissances sur le Québec!");
                         message.channel.send("Voici le leaderboard de cette partie. Ou plutôt le tableau de classement parce que je suis québécois.");
+                        message.channel.send("**TABLEAU DE CLASSEMENT**");
+                        for (var player of playerArray) {
+                            message.channel.send(`**${player.username}**: ${player.score} points`);
+                        }
                     }
                 });
             });
+            delete quiz[randomIndex];
         }
     }
 }
